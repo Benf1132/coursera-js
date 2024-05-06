200bdd7dc335f64a9d3d29daf3f67ee8d4834125
Benf1132 <122704289+Benf1132@users.noreply.github.com>
2024-05-06 18:23:15 -0400

    Update script.js

--- projects/bookApp/script.js
+++ projects/bookApp/script.js
@@ -5,15 +5,17 @@ const appSettings = {
     databaseURL: "https://best-reads-afeb4-default-rtdb.firebaseio.com/"
 }
 
+console.log("hi")
+console.log(8+8)
+
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const booksInDB = ref(database, "books")
 
 onValue(booksInDB, function(snapshot) {
     console.log(snapshot)
-    console.log("hi")
-    console.log(8+8)
 })
+
 
 const booksEl = document.getElementById("books")
 
