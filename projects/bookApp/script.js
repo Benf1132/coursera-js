eb7ad92a14081ff3cf9cc374405ca9dd88800872
Benf1132 <122704289+Benf1132@users.noreply.github.com>
2024-05-06 18:17:18 -0400

    added projects

---
+++ projects/bookApp/script.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://best-reads-afeb4-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const booksInDB = ref(database, "books")
+
+onValue(booksInDB, function(snapshot) {
+    console.log(snapshot)
+})
+
+const booksEl = document.getElementById("books")
+
+function clearBooksListEl() {
+    booksEl.innerHTML = ""
+}
+
+function appendBookToBooksListEl(bookValue) {
+    booksEl.innerHTML += `<li>${bookValue}</li>`
+}
